fc90c941260ee4cbc0699b5935a2b7ff690db7bb
abdullah cihan <abdullahcihan21@gmail.com>
2026-03-16 23:47:11 +0300

    Add numpy operations to ders_1.py

---
+++ ders_1.py
@@ -0,0 +1,7 @@
+import numpy as np
+np.random.seed(42)
+test_sonuclari=np.random.randint(0,100,size=(4,5)).copy()
+gecen_notlar=test_sonuclari[test_sonuclari>50]
+gecen_notlar+=10
+gecen_notlar.mean()
+test_sonuclari.mean()
